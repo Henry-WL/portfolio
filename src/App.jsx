b4510c88a80f162c42447e847cff88e46f11d34b
Henry <henrywl@gmail.com>
2024-03-19 01:52:50 +0000

    images

--- src/App.jsx
+++ src/App.jsx
@@ -5,96 +5,101 @@ import projects from "./data/projects.json";
 import { useState } from "react";
 
 function App() {
-  const [dark, setDark] = useState(true)
+  const [dark, setDark] = useState(false);
 
   const darkToggle = () => {
-    console.log(dark)
-    setDark(!dark)
-  }
+    console.log(dark);
+    setDark(!dark);
+  };
 
   return (
-    <div className={`${dark && 'dark'}`}>
-    <main className="dark:bg-black">
-      <section>
-        <nav className="py-10 mb-12 flex justify-around">
-          <h1 className="text-3xl dark:text-white">Henry</h1>
-          <ul className="flex items-center space-x-4">
-            <li>
-              <a href="#" className="dark:text-white">About</a>
-            </li>
-            <li>
-              <a href="#" className="dark:text-white">Projects</a>
-            </li>
-            <li onClick={darkToggle}>
-              <BsFillMoonStarsFill className="dark:bg-white" />
-            </li>
-          </ul>
-        </nav>
-      </section>
+    <div className={`${dark && "dark"}`}>
+      <main className="dark:bg-black">
+        <section>
+          <nav className="py-10 mb-12 flex justify-around">
+            <h1 className="text-3xl dark:text-white">Henry</h1>
+            <ul className="flex items-center space-x-4">
+              <li>
+                <a href="#" className="dark:text-white">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="dark:text-white">
+                  Projects
+                </a>
+              </li>
+              <li onClick={darkToggle}>
+                <BsFillMoonStarsFill className="dark:bg-white" />
+              </li>
+            </ul>
+          </nav>
+        </section>
 
-      <section>
-        <div className="text-center">
-          <h2 className="text-blue-500 text-2xl py-2">
-            Full Stack Software Engineer
-          </h2>
-          <h3 className="dark:text-white">
-            Hello! I'm a recent graduate from the Northcoders bootcamp in
-            Manchester. I've built multiple full-stack projects, take a look
-            below!
-          </h3>
+        <section>
+          <div className="text-center">
+            <h2 className="text-blue-500 text-2xl py-2">
+              Full Stack Software Engineer
+            </h2>
+            <h3 className="dark:text-white">
+              Hello! I'm a recent graduate from the Northcoders bootcamp in
+              Manchester. I've built multiple full-stack projects, take a look
+              below!
+            </h3>
 
-          <div className="flex items-center justify-center text-2xl gap-10 py-5">
-            <BsGithub className="dark:bg-white"/>
-            <BsLinkedin className="dark:bg-white"/>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-center py-4 flex flex-wrap justify-center gap-5">
-        {/* <h2 className="text-2xl">Projects</h2> */}
-
-        {projects.map((project) => {
-          return (
-            <div className="text-center shadow-lg p-10 rounded-xl border-gray-900 min-w-96 dark:bg-slate-600">
-              <div class="flex justify-center items-center"> 
-              <img
-                src={project.projectImage}
-                width={100}
-                height={100}
-                alt={project.projectName}
-                className="items-center"
-              />
-    </div> 
-              
-              <h3 className="text-lg font-medium">{project.projectName}</h3>
-
-              <p className="py-2">{project.projectDescription}</p>
-
-              <h4 className="py-4 text-teal-600">Tech staxck</h4>
-
-              {project.projectTechStack.map((tech) => {
-                return <p className="text-gray-800 py-1">{tech}</p>;
-              })}
-
-              <div className="flex justify-center gap-10 pt-5">
-                <div className="border-2 border-blue-500 rounded-md p-2">
-                <p>Github</p>
-
-                </div>
-
-                <div className="border-2 border-blue-500 rounded-md p-2">
-                <p>Demo</p>
-                </div>
-
-              </div>
+            <div className="flex items-center justify-center text-2xl gap-10 py-5">
+              <BsGithub className="dark:bg-white" />
+              <BsLinkedin className="dark:bg-white" />
             </div>
-          );
-        })}
-      </section>
-    </main>
+          </div>
+        </section>
 
+        <section className="text-center py-4 flex flex-wrap justify-center gap-5">
+          {/* <h2 className="text-2xl">Projects</h2> */}
+
+          {projects.map((project) => {
+            return (
+              <div className="text-center shadow-lg p-10 rounded-xl max-w-96 min-h-44 border-gray-900 min-w-96 dark:bg-slate-600">
+                <div class="flex justify-center items-center">
+                  <img
+                    src={project.projectImage}
+                    width={350}
+                    height={150}
+                    alt={project.projectName}
+                    className="object-contain rounded-md h-60"
+                  />
+                </div>
+
+                <h3 className="text-lg font-medium">{project.projectName}</h3>
+
+                <p className="py-2">{project.projectDescription}</p>
+
+                <h4 className="py-4 text-teal-600">Tech stack</h4>
+
+                {project.projectTechStack.map((tech) => {
+                  return <p className="text-gray-800 py-1">{tech}</p>;
+                })}
+
+                <div className="flex justify-center gap-10 pt-5">
+                  <div className="border-2 border-blue-500 rounded-md p-2">
+                    <a href={project.projectGithub} target="_blank">
+                      Github
+                    </a>
+                  </div>
+
+                  <div className="border-2 border-blue-500 rounded-md p-2">
+                    <a href={project.projectDemo} target="_blank">
+                      Demo
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </main>
     </div>
-     );
+  );
 }
 
 export default App;
